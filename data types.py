b54134ec2774340cb23bd830b21c52c4fc827afb
Viraaj-ai <vintejvir@gmail.com>
2025-04-08 17:10:27 +0530

    data types

--- data types.py
+++ data types.py
@@ -6,3 +6,7 @@ c="welcome to Python"
 print("type of data:",type(c))
 d=True
 print("type of data:",type(d))
+a=int(a)
+b=str(b)
+print("type of data:",type(a))
+print("type of data:",type(b))
---
+++ slicing.py
@@ -0,0 +1,3 @@
+text=str(input("Enter any word:" ))
+revtext=text[::-1]
+print(revtext)
